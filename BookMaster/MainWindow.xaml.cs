77830f8dc37016117521d3e11c03a1cda545ea39
Lev <LevLeo2011@yandex.ru>
2021-09-17 18:06:54 +0300

    Реализовано окно входа, баг на главной странице, ищу решение.

---
+++ BookMaster/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using BookMaster.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookMaster
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        
+        {
+        
+            InitializeComponent();
+        
+        }
+
+        private void MnIt_Login_Click(object sender, RoutedEventArgs e)
+  
+        {
+
+            Autorize.AccesLibrary = MnItLibrary; //record menu "Library" in class
+            Windows.WindowDilogLogin dilogLogin = new Windows.WindowDilogLogin();
+            dilogLogin.Show();                                                      //open login dialog
+
+
+        }
+
+        private void MnIt_LogOut_Click(object sender, RoutedEventArgs e)
+        
+        {
+            
+            MainWindow mainWindow = new MainWindow();  //LogOut
+            this.Close();
+            mainWindow.Show();
+            
+        }
+
+        private void MnIt_Close_Click(object sender, RoutedEventArgs e)
+        
+        {
+        
+            Application.Current.Shutdown(); //Shutdown application
+        
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ BookMaster/Windows/WindowDilogLogin.xaml.cs
@@ -0,0 +1,43 @@
+﻿using BookMaster.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookMaster.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowDilogLogin.xaml
+    /// </summary>
+    public partial class WindowDilogLogin : Window
+    {
+        public WindowDilogLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+
+            Autorize.AccesLibrary.IsEnabled = true;
+            this.Close();
+
+        }
+
+        private void Btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.Close();
+
+        }
+    }
+}
